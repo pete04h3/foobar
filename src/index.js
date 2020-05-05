b46f1866f91beb012b93804bbe2c456d5d71f0b8
peterpaN1k <peetmj@gmail.com>
2020-05-05 17:51:38 +0200

    New Chuck Norris joke fetched every 10second

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  state = {
+    data: "",
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() {
+    this.getData();
+    this.interval = setInterval(() => {
+      this.getData();
+    }, 5000);
+  }
+
+  getData() {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        this.setState({
+          data: res.value,
+        });
+      });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <p>{this.state.data}</p>;
+  }
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
