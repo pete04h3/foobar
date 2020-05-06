ecca6ee972f0f914291128306aa88548e9f1cf78
peterpaN1k <peetmj@gmail.com>
2020-05-06 14:15:16 +0200

    Updates

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", init);
-const spinningNumber = document.querySelector("#root > div > article > h5");
 
 function init() {
-  console.log(init);
-
+  const spinningNumber = document.querySelector("#root > div > article > h5");
   spinningNumber.classList.add("spinner");
 }
